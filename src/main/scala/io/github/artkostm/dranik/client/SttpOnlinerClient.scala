95d2ce2c76cae5d81084cd340897f0f100ad85b7
Artsiom Chuiko <artkostm@gmail.com>
2021-01-03 02:04:05 +0300

    add client

---
+++ src/main/scala/io/github/artkostm/dranik/client/SttpOnlinerClient.scala
@@ -0,0 +1,24 @@
+package io.github.artkostm.dranik.client
+
+import com.github.plokhotnyuk.jsoniter_scala.macros._
+import com.github.plokhotnyuk.jsoniter_scala.core._
+import io.github.artkostm.dranik.client.OnlinerClient.{ApartmentsResponse, PkApiRequest}
+import zio.{Has, Schedule, Task}
+import sttp.client3._
+import zio.clock.Clock
+import zio.duration.Duration
+
+class SttpOnlinerClient(config: ClientConfig)(backend: OnlinerBackend) extends OnlinerClient.Service {
+
+
+  override def getApartments(r: PkApiRequest): Task[ApartmentsResponse] =
+    basicRequest.get(
+      uri"${config.url}/search/apartments?page=${r.page}&bounds[lb][lat]=${r.lb.map(_.lat)}&bounds[lb][long]=${r.lb.map(_.lng)}&bounds[rt][lat]=${r.rb.map(_.lat)}&bounds[rt][long]=${r.rb.map(_.lng)}"
+    )
+      .response(asByteArray.getRight.map(bytes => readFromArray[ApartmentsResponse](bytes))) // todo: abstract using protocols, avoid using jsoniter here
+      .send(backend)
+      .map(_.body)
+      .retry(Schedule.recurs(config.retry.times) && Schedule.spaced(Duration.fromScala(config.retry.every)))
+      .provide(Has(Clock.Service.live))
+
+}
---
+++ src/main/scala/io/github/artkostm/dranik/client/package.scala
@@ -0,0 +1,44 @@
+package io.github.artkostm.dranik
+
+import com.github.plokhotnyuk.jsoniter_scala.core.JsonValueCodec
+import com.github.plokhotnyuk.jsoniter_scala.macros.JsonCodecMaker
+import sttp.capabilities.WebSockets
+import sttp.capabilities.zio.ZioStreams
+import sttp.client3.SttpBackend
+import zio.{Has, Task, ZLayer}
+
+import scala.concurrent.duration._
+
+package object client {
+  final case class RetryConfig(times: Int, every: Duration)
+  final case class ClientConfig(url: String, auth: Option[Any] = None, retry: RetryConfig = RetryConfig(0, 0 seconds))
+
+  type OnlinerClient  = Has[OnlinerClient.Service]
+  type OnlinerBackend = SttpBackend[Task, ZioStreams with WebSockets]
+
+  object OnlinerClient {
+    trait Service {
+      def getApartments(request: PkApiRequest): Task[ApartmentsResponse]
+    }
+
+    lazy val live = ZLayer.fromServices[OnlinerBackend, ClientConfig, OnlinerClient.Service] {
+      (backend, config) => new SttpOnlinerClient(config)(backend)
+    }
+
+    // request
+    final case class LatLngRequest(lat: BigDecimal, lng: BigDecimal)
+    final case class PkApiRequest(page: Option[Int] = Some(1), lb: Option[LatLngRequest], rb: Option[LatLngRequest])
+    // response
+    final case class PageResponse(limit: Int, items: Int, current: Int, last: Int)
+    final case class LocationResponse(address: String, user_address: String, latitude: Double, longitude: Double)
+    final case class ConversionResponse(amount: String, currency: String)
+    final case class PriceResponse(amount: String, currency: String, converted: Map[String, ConversionResponse])
+    final case class SellerResponse(`type`: String)
+    final case class ApartmentResponse(id: Long, author_id: Long, location: LocationResponse, price: PriceResponse, resale: Boolean, number_of_rooms: Int, floor: Int, number_of_floors: Int, area: Map[String, Option[Double]], photo: String, seller: SellerResponse, created_at: String, last_time_up: String, up_available_in: Long, url: String, auction_bid: Map[String, String])
+    final case class ApartmentsResponse(apartments: Seq[ApartmentResponse], total: Int, page: PageResponse)
+
+    object ApartmentsResponse {
+      implicit val apartmentsCodec: JsonValueCodec[ApartmentsResponse] = JsonCodecMaker.make
+    }
+  }
+}
