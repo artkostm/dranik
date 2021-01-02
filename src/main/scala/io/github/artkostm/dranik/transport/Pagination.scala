060fe276fd899b0e59bd85a5913549109d90c469
Artsiom Chuiko <artkostm@gmail.com>
2021-01-03 02:06:06 +0300

    add pagination

---
+++ src/main/scala/io/github/artkostm/dranik/transport/Pagination.scala
@@ -0,0 +1,16 @@
+package io.github.artkostm.dranik.transport
+
+import io.github.artkostm.dranik.client.OnlinerClient.ApartmentsResponse
+import zio.Task
+
+trait Pagination {
+  def apartmentsPage(call: Int => Task[ApartmentsResponse]): Task[Unit] = {
+    def loop(page: Int): Task[Unit] =
+      call(page).flatMap {
+        case response if response.page.last > page => loop(page + 1)
+        case _                                     => Task.unit
+      }
+
+    loop(1)
+  }
+}
