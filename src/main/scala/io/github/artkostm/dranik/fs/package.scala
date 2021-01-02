a64ad6af063838fb03f0a8d14d5b01476b6349ea
Artsiom Chuiko <artkostm@gmail.com>
2021-01-03 02:04:55 +0300

    add FS interface

---
+++ src/main/scala/io/github/artkostm/dranik/fs/package.scala
@@ -0,0 +1,57 @@
+package io.github.artkostm.dranik
+
+import com.microsoft.azure.storage.CloudStorageAccount
+import com.microsoft.azure.storage.blob.CloudBlobContainer
+import io.github.artkostm.dranik.Cli.TransportAppConfig
+import zio.{Has, Managed, Task, URLayer, ZLayer}
+import zio.stream.{Sink, Stream}
+
+import java.io.{InputStream, OutputStream}
+import java.nio.ByteBuffer
+
+package object fs {
+  type FS = Has[FS.Service]
+
+  object FS {
+
+    trait Service {
+      def write(fileName: String, bytes: Array[Byte]): Task[String] =
+        writeStream(fileName) { writer =>
+          Task(writer.write(bytes))
+        }
+
+      def write(fileName: String, stream: Stream[Throwable, ByteBuffer]): Task[String] =
+        writeStream(fileName) { writer =>
+          stream.run(
+            Sink.foldLeftM[Any, Throwable, ByteBuffer, OutputStream](writer) { (w, c) =>
+              Task(w.write(c.array())).map(_ => w)
+            }
+          )
+        }
+
+      def writeStream(fileName: String)(resource: OutputStream => Task[Any]): Task[String]
+      def read(fileName: String): Managed[Throwable, (Long, InputStream)]
+    }
+
+    def azureBlobContainer: ZLayer[Has[TransportAppConfig], Nothing, Has[CloudBlobContainer]] =
+      ZLayer.fromServiceM[TransportAppConfig, Any, Nothing, CloudBlobContainer] { config =>
+        Task {
+          val credentials = config.storageAccount.zip(config.storageAccountKey)
+            .getOrElse(throw new RuntimeException("Cannot connect to ADLS"))
+          val storageAccount = CloudStorageAccount.parse(
+            s"DefaultEndpointsProtocol=https;AccountName=${credentials._1.value};AccountKey=${credentials._2.value}"
+          )
+          val blobClient = storageAccount.createCloudBlobClient()
+          blobClient.getContainerReference(config.container.getOrElse(throw new RuntimeException("Please specify the Storage container name")))
+        }.orDie
+      }
+
+    def azure: URLayer[Has[TransportAppConfig] with Has[CloudBlobContainer], FS] =
+      ZLayer.fromServices[TransportAppConfig, CloudBlobContainer, FS.Service] { (config, container) =>
+        new AzureBlob(container, config.blob.getOrElse(throw new RuntimeException("Please specify root blob")))
+      }
+
+    def local: URLayer[Has[TransportAppConfig], FS] =
+      ZLayer.fromService[TransportAppConfig, FS.Service](config => new Local(config.localDir.getOrElse(throw new RuntimeException("Please specify local root dir for debug"))))
+  }
+}
